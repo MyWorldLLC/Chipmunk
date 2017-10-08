89fb6f002d4cb053ce5674b4b55fecbbc506df29
Daniel Perano <51095634+danielperano@users.noreply.github.com>
2017-10-08 14:31:05 -0700

    Make assignment right associative.

--- src/main/java/chipmunk/compiler/parselets/AssignOperatorParselet.java
+++ src/main/java/chipmunk/compiler/parselets/AssignOperatorParselet.java
@@ -3,6 +3,10 @@ package chipmunk.compiler.parselets;
 import chipmunk.compiler.OperatorPrecedence;
 
 public class AssignOperatorParselet extends BaseBinaryOperatorParselet {
+	
+	public AssignOperatorParselet(){
+		super(false);
+	}
 
 	@Override
 	public int getPrecedence() {
