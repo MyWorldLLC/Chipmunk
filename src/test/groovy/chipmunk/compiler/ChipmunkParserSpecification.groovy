ab9f8d6146e3535ede62cd9bb4a592e26122b6dc
Daniel Perano <51095634+danielperano@users.noreply.github.com>
2017-10-08 14:38:33 -0700

    Test right-associativity.

--- src/test/groovy/chipmunk/compiler/ChipmunkParserSpecification.groovy
+++ src/test/groovy/chipmunk/compiler/ChipmunkParserSpecification.groovy
@@ -56,5 +56,31 @@ class ChipmunkParserSpecification extends Specification {
 		then:
 		ast.toString() == "(* (+ (literal 1)(literal 2))(literal 3))"
 	}
+	
+	def "parse 1 ** 2 ** 3"(){
+		setup:
+		def lexer = new ChipmunkLexer()
+		lexer.lex("1 ** 2 ** 3")
+		def parser = new ChipmunkParser(lexer.getLastTokens())
+		
+		when:
+		AstNode ast = parser.parseExpression()
+		
+		then:
+		ast.toString() == "(** (literal 1)(** (literal 2)(literal 3)))"
+	}
+	
+	def "parse (1 ** 2) ** 3"(){
+		setup:
+		def lexer = new ChipmunkLexer()
+		lexer.lex("(1 ** 2) ** 3")
+		def parser = new ChipmunkParser(lexer.getLastTokens())
+		
+		when:
+		AstNode ast = parser.parseExpression()
+		
+		then:
+		ast.toString() == "(** (** (literal 1)(literal 2))(literal 3))"
+	}
 
 }
