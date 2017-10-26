70352e3b5ecc9513ed1e6a3001f1f80ec5706dc0
Daniel Perano <51095634+danielperano@users.noreply.github.com>
2017-10-26 16:51:45 -0700

    Misc. test cleanup, added a number of math tests to CIntSpecification.

--- src/test/groovy/chipmunk/compiler/AssemblerSpecification.groovy
+++ src/test/groovy/chipmunk/compiler/AssemblerSpecification.groovy
@@ -2,29 +2,36 @@ package chipmunk.compiler
 
 import chipmunk.ChipmunkContext
 import chipmunk.modules.lang.CCode
+import chipmunk.modules.lang.CFloat
 import chipmunk.modules.lang.CInt
 import chipmunk.modules.lang.CMethod
 import chipmunk.modules.lang.CObject
 import spock.lang.Specification
 
 class AssemblerSpecification extends Specification {
+	
+	ChipmunkAssembler assembler = new ChipmunkAssembler()
 
 	def "Assemble and run 1 + 2"(){
 		when:
-		ChipmunkAssembler assembler = new ChipmunkAssembler()
 		assembler.push(new CInt(1))
 		assembler.push(new CInt(2))
 		assembler.add()
 		assembler._return()
 		
-		CMethod method = new CMethod()
-		method.setCode(new CCode(assembler.getCodeSegment()))
-		method.setConstantPool(assembler.getConstantPool())
-		
-		CObject result = method.__call__(new ChipmunkContext(), 0, false)
+		def result = callMethod()
 		
 		then:
 		result instanceof CInt
 		result.getValue() == 3
+	}
+	
+	def callMethod(){
+		
+		CMethod method = new CMethod()
+		method.setCode(new CCode(assembler.getCodeSegment()))
+		method.setConstantPool(assembler.getConstantPool())
+		
+		return method.__call__(new ChipmunkContext(), 0, false)
 	}
 }
--- src/test/groovy/chipmunk/modules/lang/CIntSpecification.groovy
+++ src/test/groovy/chipmunk/modules/lang/CIntSpecification.groovy
@@ -3,29 +3,26 @@ package chipmunk.modules.lang
 import spock.lang.Specification
 
 class CIntSpecification extends Specification {
+	
+	CInt one = new CInt(1)
+	CInt two = new CInt(2)
+	CInt three = new CInt(3)
+	
+	CFloat fTwo = new CFloat(2.0)
 
 	def "create CInt with default value"(){
-		when:
-		CInt i = new CInt()
-		
-		then:
-		i.getValue() == 0
+		expect:
+		new CInt().getValue() == 0
 	}
 	
 	def "create CInt with specified value"(){
-		when:
-		CInt i = new CInt(1)
-		
-		then:
-		i.getValue() == 1
+		expect:
+		new CInt(1).getValue() == 1
 	}
 	
 	def "add 1 + 2"(){
 		when:
-		CInt first = new CInt(1)
-		CInt second = new CInt(2)
-		
-		def result = first.__plus__(second)
+		def result = one.__plus__(two)
 		
 		then:
 		result instanceof CInt
@@ -34,10 +31,7 @@ class CIntSpecification extends Specification {
 	
 	def "add 1 + 2.0"(){
 		when:
-		CInt first = new CInt(1)
-		CFloat second = new CFloat(2.0)
-		
-		def result = first.__plus__(second)
+		def result = one.__plus__(fTwo)
 		
 		then:
 		result instanceof CFloat
@@ -46,13 +40,64 @@ class CIntSpecification extends Specification {
 	
 	def "add 1 + CString"(){
 		when:
-		CInt first = new CInt(1)
-		CString second = new CString("foo")
-		
-		def result = first.__plus__(second)
+		def result = new CInt(1).__plus__(new CString("foo"))
 		
 		then:
 		def ex = thrown(UnimplementedOperationChipmunk)
 		ex.getMessage() == "Undefined operation: cannot perform int + CString"
+	}
+	
+	def "perform 1 * 2"(){
+		when:
+		def result = one.__mul__(two)
+		
+		then:
+		result instanceof CInt
+		result.getValue() == 2
+	}
+	
+	def "perform 1 * 2.0"(){
+		when:
+		def result = one.__mul__(fTwo)
+		
+		then:
+		result instanceof CFloat
+		result.getValue() == 2.0
+	}
+	
+	def "perform 1 / 2"(){
+		when:
+		def result = one.__div__(two)
+		
+		then:
+		result instanceof CFloat
+		result.getValue() == 0.5
+	}
+	
+	def "perform 1 / 2.0"(){
+		when:
+		def result = one.__div__(fTwo)
+		
+		then:
+		result instanceof CFloat
+		result.getValue() == 0.5
+	}
+	
+	def "perform 1 // 2"(){
+		when:
+		def result = one.__fdiv__(two)
+		
+		then:
+		result instanceof CInt
+		result.getValue() == 0
+	}
+	
+	def "perform 1 // 2.0"(){
+		when:
+		def result = one.__fdiv__(fTwo)
+		
+		then:
+		result instanceof CInt
+		result.getValue() == 0
 	}
 }
