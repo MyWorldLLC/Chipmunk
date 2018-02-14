1b405acad6dec8e3ee6ea9b985146a383e63d719
Daniel Perano <51095634+danielperano@users.noreply.github.com>
2018-02-13 16:03:32 -0800

    Added ChipmunkProfiler application to facilitate easy inspection of Chipmunk execution with profiling tools.

---
+++ src/profiling/java/chipmunk/profiling/ChipmunkProfiler.java
@@ -0,0 +1,54 @@
+package chipmunk.profiling;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import chipmunk.ChipmunkVM;
+import chipmunk.compiler.ChipmunkLexer;
+import chipmunk.compiler.ChipmunkParser;
+import chipmunk.compiler.ast.AstNode;
+import chipmunk.compiler.codegen.MethodVisitor;
+import chipmunk.compiler.codegen.SymbolTableBuilderVisitor;
+import chipmunk.modules.reflectiveruntime.CMethod;
+
+public class ChipmunkProfiler {
+	
+	public static CMethod compileTestMethod(InputStream is) throws Exception {
+		StringBuilder builder = new StringBuilder();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		
+		String last = reader.readLine();
+		while(last != null){
+			builder.append(last);
+			last = reader.readLine();
+		}
+		
+		ChipmunkLexer lexer = new ChipmunkLexer();
+		ChipmunkParser parser = new ChipmunkParser(lexer.lex(builder.toString()));
+		
+		AstNode root = parser.parseMethodDef();
+		root.visit(new SymbolTableBuilderVisitor());
+		
+		List<Object> constantPool = new ArrayList<Object>();
+		MethodVisitor visitor = new MethodVisitor(constantPool);
+		
+		root.visit(visitor);
+		return visitor.getMethod();
+	}
+	
+	public static void main(String[] args) throws Exception{
+		
+		CMethod countToAMillion = compileTestMethod(ChipmunkProfiler.class.getResourceAsStream("CountToAMillion.chp"));
+		ChipmunkVM vm = new ChipmunkVM();
+		
+		System.out.println("Starting profiler. Press Ctrl-C to exit.");
+		while(true){
+			vm.dispatch(countToAMillion, 0);
+		}
+		
+	}
+
+}
