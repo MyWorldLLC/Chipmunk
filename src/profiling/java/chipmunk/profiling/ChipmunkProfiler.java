d1025964987c89c34200169f3b0476e5be7c42b9
Daniel Perano <51095634+danielperano@users.noreply.github.com>
2020-02-29 00:44:01 -0800

    Fixed profiler

--- src/profiling/java/chipmunk/profiling/ChipmunkProfiler.java
+++ src/profiling/java/chipmunk/profiling/ChipmunkProfiler.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 
 import chipmunk.ChipmunkScript;
 import chipmunk.ChipmunkVM;
+import chipmunk.modules.runtime.CMethod;
 
 public class ChipmunkProfiler {
 	
@@ -12,20 +13,29 @@ public class ChipmunkProfiler {
 	}
 	
 	public static void main(String[] args) throws Exception{
-		
-		ChipmunkScript countToAMillion = compileScript(ChipmunkProfiler.class.getResourceAsStream("CountToAMillion.chp"), "countToAMillion");
-		countToAMillion.setEntryCall("profiling", "countToAMillion");
-		
-		ChipmunkScript fibonacci = compileScript(ChipmunkProfiler.class.getResourceAsStream("Fibonacci.chp"), "fibonacci");
-		countToAMillion.setEntryCall("profiling", "main");
-		
+
 		ChipmunkVM vm = new ChipmunkVM();
+		ChipmunkScript countToAMillion = compileScript(ChipmunkProfiler.class.getResourceAsStream("CountToAMillion.chp"), "countToAMillion");
+		vm.run(countToAMillion);
+
+		ChipmunkScript fibonacci = compileScript(ChipmunkProfiler.class.getResourceAsStream("Fibonacci.chp"), "fibonacci");
+		// Run once to initialize and load all modules
+		vm.run(fibonacci);
+
+		// After modules are initialized and one run has completed, get the main method and invoke repeatedly
+		CMethod mainMethod = (CMethod) fibonacci
+				.getModules()
+				.get("profiling")
+				.getNamespace()
+				.get("main");
+		
+
 		
 		System.out.println("Starting profiler. Press Ctrl-C to exit.");
 		while(true){
 			Object value;
 			long startTime = System.nanoTime();
-			value = vm.run(fibonacci);
+			value = vm.dispatch(mainMethod, null);
 			long endTime = System.nanoTime();
 			
 			System.out.println("Value: " + value.toString() + ", Time: " + (endTime - startTime) / 1e9 + " seconds");
