3117f04f03f96ecff7987dbd78489cef6240736d
danielperano <51095634+danielperano@users.noreply.github.com>
2020-11-10 18:55:44 -0800

    Remove legacy binding code

--- src/main/java/chipmunk/vm/invoke/Binder.java
+++ src/main/java/chipmunk/vm/invoke/Binder.java
@@ -34,8 +34,6 @@ public class Binder {
     protected static final ChipmunkLinker chipmunkLinker = new ChipmunkLinker();
     protected static final DynamicLinker dynaLink = createDynamicLinker();
 
-    protected final MethodHandles.Lookup methodLookup;
-
     public static CallSite bootstrapCallsite(MethodHandles.Lookup lookup, String name, MethodType callType) throws NoSuchMethodException, IllegalAccessException {
         return dynaLink.link(new SimpleRelinkableCallSite(
                 new CallSiteDescriptor(lookup, chipmunkCallOp(name), callType)
@@ -81,113 +79,6 @@ public class Binder {
         chipmunkLinker.getLibraries().registerLibrary(new NativeTypeLib());
 
         return factory.createLinker();
-    }
-
-    public Binder(){
-
-        methodLookup = MethodHandles.lookup();
-    }
-
-/*    public Object lookupMethod(Object target, String opName, Class<?>[] paramTypes) throws Throwable {
-
-        CallSignature signature = new CallSignature(target.getClass(), opName, paramTypes);
-        Object callTarget = cache.getTarget(signature);
-        if(callTarget != null){
-            return callTarget;
-        }
-
-        Method[] methods = target.getClass().getMethods();
-        Method method = null;
-        for (int i = 0; i < methods.length; i++) {
-            if (methods[i].getName().equals(opName)) {
-                // only call public methods
-                if (paramTypesMatch(methods[i].getParameterTypes(), paramTypes)
-                        && ((methods[i].getModifiers() & Modifier.PUBLIC) != 0)) {
-
-                    method = methods[i];
-                    break;
-                }
-            }
-        }
-
-        if(method == null) {
-            throw new NoSuchMethodException(formatMissingMethodMessage(target.getClass(), opName, paramTypes));
-        }
-
-        Class<?> callTypeClass = null;
-        if(paramTypes.length < 11) {
-            // direct-bind method
-
-            if(method.getReturnType().equals(void.class)) {
-                callTypeClass = this.voidCallTypes[paramTypes.length];
-            }else {
-                callTypeClass = this.callTypes[paramTypes.length];
-            }
-
-            try {
-                MethodHandle implementationHandle = methodLookup.unreflect(method);
-
-                MethodType interfaceType = MethodType.methodType(callTypeClass);
-                MethodType implType = MethodType.methodType(method.getReturnType(), target.getClass()).appendParameterTypes(paramTypes);
-
-                callTarget = LambdaMetafactory.metafactory(
-                        methodLookup,
-                        "call",
-                        interfaceType,
-                        implType.erase(),
-                        implementationHandle,
-                        implementationHandle.type())
-                        .getTarget().invoke();
-            } catch (IllegalAccessException | LambdaConversionException e) {
-                throw e;
-            }
-
-        }else {
-            // non-statically bind method
-            try {
-                callTarget = methodLookup.unreflect(method).asSpreader(1, Object[].class, paramTypes.length);
-            } catch (IllegalAccessException e) {
-                throw new NoSuchMethodException(formatMissingMethodMessage(target.getClass(), opName, paramTypes));
-            }
-        }
-
-        cache.cacheTarget(signature, callTarget);
-        return callTarget;
-    }*/
-
-    private boolean paramTypesMatch(Class<?>[] targetTypes, Class<?>[] callTypes) {
-
-        if (targetTypes.length != callTypes.length) {
-            return false;
-        }
-
-        for (int i = 0; i < targetTypes.length; i++) {
-            if (targetTypes[i] != callTypes[i]) {
-                if (!targetTypes[i].isAssignableFrom(callTypes[i])) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    private String formatMissingMethodMessage(Class<?> targetType, String methodName, Class<?>[] paramTypes) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("No suitable method found: ");
-        sb.append(targetType.getName());
-        sb.append('.');
-        sb.append(methodName);
-        sb.append('(');
-
-        for (int i = 0; i < paramTypes.length; i++) {
-            sb.append(paramTypes[i].getName());
-            if (i < paramTypes.length - 1) {
-                sb.append(',');
-            }
-        }
-        sb.append(')');
-        return sb.toString();
     }
 
 }
