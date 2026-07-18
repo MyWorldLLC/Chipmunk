49beca86f003977ceb5e849f62e2d57e9a07da70
danielperano <51095634+danielperano@users.noreply.github.com>
2026-07-17 18:00:56 -0700

    Support passing program name to profiler, bump fib recursion to 30

--- Lang/src/profiling/java/chipmunk/profiling/ChipmunkProfiler.java
+++ Lang/src/profiling/java/chipmunk/profiling/ChipmunkProfiler.java
@@ -23,27 +23,33 @@ package chipmunk.profiling;
 import chipmunk.vm.ChipmunkScript;
 import chipmunk.vm.ChipmunkVM;
 
+import java.util.HashMap;
+
 public class ChipmunkProfiler {
 
 	
 	public static void main(String[] args) throws Throwable {
 
 		ChipmunkVM vm = new ChipmunkVM();
-		ChipmunkScript countToAMillion = vm.compileScript(ChipmunkProfiler.class.getResourceAsStream("CountToAMillion.chp"), "countToAMillion");
-		ChipmunkScript countingForLoop = vm.compileScript(ChipmunkProfiler.class.getResourceAsStream("CountingForLoop.chp"), "countingForLoop");
-
-		ChipmunkScript fibonacci = vm.compileScript(ChipmunkProfiler.class.getResourceAsStream("Fibonacci.chp"), "fibonacci");
-		ChipmunkScript mandelbrot = vm.compileScript(ChipmunkProfiler.class.getResourceAsStream("Mandelbrot.chp"), "mandelbrot");
-
-
-		ChipmunkScript polymorphism = vm.compileScript(ChipmunkProfiler.class.getResourceAsStream("PolymorphicCalling.chp"), "polymorphism");
-		ChipmunkScript nonpolymorphism = vm.compileScript(ChipmunkProfiler.class.getResourceAsStream("NonpolymorphicCalling.chp"), "nonpolymorphism");
+		var programs = new HashMap<String, ChipmunkScript>();
+		programs.put("countToAMillion", vm.compileScript(ChipmunkProfiler.class.getResourceAsStream("CountToAMillion.chp"), "countToAMillion"));
+		programs.put("countingForLoop", vm.compileScript(ChipmunkProfiler.class.getResourceAsStream("CountingForLoop.chp"), "countingForLoop"));
+		programs.put("fibonacci", vm.compileScript(ChipmunkProfiler.class.getResourceAsStream("Fibonacci.chp"), "fibonacci"));
+		programs.put("mandelbrot", vm.compileScript(ChipmunkProfiler.class.getResourceAsStream("Mandelbrot.chp"), "mandelbrot"));
+		programs.put("polymorphism", vm.compileScript(ChipmunkProfiler.class.getResourceAsStream("PolymorphicCalling.chp"), "polymorphism"));
+		programs.put("nonpolymorphism", vm.compileScript(ChipmunkProfiler.class.getResourceAsStream("NonpolymorphicCalling.chp"), "nonpolymorphism"));
 		
 		System.out.println("Starting profiler. Press Ctrl-C to exit.");
+
+		var program = programs.get("countToAMillion");
+		if(args.length > 0){
+			program = programs.get(args[0]);
+		}
+
 		while(true){
 			Object value;
 			long startTime = System.nanoTime();
-			value = vm.runAsync(polymorphism).get();
+			value = vm.runAsync(program).get();
 			long endTime = System.nanoTime();
 			
 			System.out.println("Value: " + value + ", Time: " + (endTime - startTime) / 1e9 + " seconds");
